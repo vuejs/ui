d95a8dbd9c0b65a167d4b02e6319ee28caab124f
Guillaume Chau <alphadelta.fg@gmail.com>
2018-03-01 12:10:51 +0100

    Demo build

--- docs-src/main.js
+++ docs-src/main.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import VueUiFramework from '../src'
+import VueUiFramework from '../'
+import '../dist/vue-ui.css'
 import './components'
 
 Vue.use(VueUiFramework)
