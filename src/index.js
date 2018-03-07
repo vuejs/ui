e518fb319087b4d99b9ee3e1ff3f5c1f0bc7e404
Guillaume Chau <alphadelta.fg@gmail.com>
2018-03-07 19:12:42 +0100

    fix: tooltips boundaries config + exports mixins

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import VTooltip from 'v-tooltip'
 import VueResize from 'vue-resize'
 import 'focus-visible'
 
+// Exported mixins
+export { default as CoupledChild } from './mixins/CoupledChild'
+export { default as CoupledParent } from './mixins/CoupledParent'
+export { default as DisabledChild } from './mixins/DisabledChild'
+export { default as DisableScroll } from './mixins/DisableScroll'
+
 // Require all the components that start with 'BaseXXX.vue'
 const components = require.context('./components', true, /[a-z0-9]+\.vue$/)
 
@@ -11,6 +17,7 @@ export function install (Vue, options = {}) {
 
   Vue.use(VTooltip, Object.assign({
     defaultDelay: { show: 1000, hide: 0 },
+    defaultBoundariesElement: document.body,
   }, options.vtooltip))
 
   Vue.use(VueResize)
